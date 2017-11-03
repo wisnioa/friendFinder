1e6323568f0ec4a7f57dd0e614396a6108182ccc
unknown <amanda.wisniowski@gmail.com>
2017-11-02 21:40:50 -0500

    fixed express port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var app = express();
-var PORT = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -41,6 +41,6 @@ require('./app/routing/htmlRoutes.js')(app);
 // });
 
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
 });
